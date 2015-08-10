e8a6bc2dfcd62bf069a61daf6ac5d6cfce5d9992
liguiqing@gmail.com <liguiqing@gmail.com>
2015-08-11 00:08:49 +0800

    lautout2

--- joy-grading/src/main/java/com/joy/grading/controller/MainController.java
+++ joy-grading/src/main/java/com/joy/grading/controller/MainController.java
@@ -1,5 +1,5 @@
 /**
- * Joy Anything (优异安心)
+ * Joy Anything (卓越安心)
  * 
  * <p><b>© 2015-2015 </b></p>
  * 
@@ -10,6 +10,7 @@ package com.joy.grading.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,11 +28,19 @@ import com.joy.commons.web.view.ModelAndViewFactory;
 public class MainController {
     private static Logger logger = LoggerFactory.getLogger(MainController.class);
 	
-	@RequestMapping(value="/index")
+	@RequestMapping(value="/")
 	public ModelAndView onIndex()throws Exception{
 		logger.debug("URL / Method Get");
 		
 		return ModelAndViewFactory.newModelAndViewFor("/index").build();
+	}
+	
+	@RequestMapping(value="/marking/{l}")
+	public ModelAndView onMark(@PathVariable String l)throws Exception{
+		logger.debug("URL /mark Method Get");
+		if("0".equals(l))
+			l="";
+		return ModelAndViewFactory.newModelAndViewFor("marking/layout"+l).build();
 	}
 }
 
