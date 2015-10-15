def1c6c47dd49b44f9b8756de08495878281e24c
newyuliuyu <306487103@qq.com>
2015-10-15 10:26:26 +0800

    编写注释

--- easytnt-imports-paper/src/main/java/com/easytnt/io/scanfiledir/DirectoryScanner.java
+++ easytnt-imports-paper/src/main/java/com/easytnt/io/scanfiledir/DirectoryScanner.java
@@ -11,7 +11,17 @@ import java.io.IOException;
  *
  */
 public interface DirectoryScanner {
+	/**
+	 * 扫描文件路径
+	 * @return
+	 * @throws IOException
+	 */
 	public FileInfoContainer scan() throws IOException;
 
+	/**
+	 * 扫描文件路径
+	 * @param visitorFile 回调函数
+	 * @throws IOException
+	 */
 	public void scan(VisitorFile visitorFile) throws IOException;
 }
