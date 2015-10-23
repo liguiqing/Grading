0a26c82832c3be21928c4e678ea9244d404c6c6a
liguiqing <liguiking@gmail.com>
2015-10-23 22:07:14 +0800

    ajax重构

---
+++ easytnt-grading/src/main/java/com/easytnt/grading/controller/TestBean.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.easytnt.grading.controller;
+
+/**
+ * <Pre>
+ * </Pre>
+ *
+ * @author liuyu
+ * @data 2015年10月23日 上午9:48:03
+ */
+public class TestBean {
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public TestBean setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public TestBean setAge(int age) {
+		this.age = age;
+		return this;
+	}
+
+}
