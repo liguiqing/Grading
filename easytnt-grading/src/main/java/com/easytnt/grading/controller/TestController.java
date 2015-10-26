aed163e6f12e0240ae682c662e8e0db33569a791
liguiqing <liguiking@gmail.com>
2015-10-27 01:42:22 +0800

    删除导入的junit包

--- easytnt-grading/src/main/java/com/easytnt/grading/controller/TestController.java
+++ easytnt-grading/src/main/java/com/easytnt/grading/controller/TestController.java
@@ -9,15 +9,11 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import org.springframework.web.servlet.ModelAndView;
 
 import com.easytnt.commons.web.view.ModelAndViewFactory;
